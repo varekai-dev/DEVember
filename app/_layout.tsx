1413ece779af218208b6953d550b14eff6f09399
Serhii <s.saviuk@apiko.com>
2024-01-23 15:24:14 +0200

    load fonts in the root layout

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,28 @@
-import { Stack } from 'expo-router'
+import {
+	AmaticSC_400Regular,
+	AmaticSC_700Bold,
+} from '@expo-google-fonts/amatic-sc'
+import { useFonts } from 'expo-font'
+import { SplashScreen, Stack } from 'expo-router'
+import { useEffect } from 'react'
+
+SplashScreen.preventAutoHideAsync()
 
 export default function RootLayout() {
+	const [fontsLoaded, fontError] = useFonts({
+		Amatic: AmaticSC_400Regular,
+		AmaticBold: AmaticSC_700Bold,
+	})
+
+	useEffect(() => {
+		if (fontsLoaded || fontError) {
+			SplashScreen.hideAsync()
+		}
+	}, [fontsLoaded, fontError])
+
+	if (!fontsLoaded && !fontError) {
+		return null
+	}
 	return (
 		<Stack>
 			<Stack.Screen
--- app/index.tsx
+++ app/index.tsx
@@ -1,33 +1,9 @@
 import { StatusBar } from 'expo-status-bar'
 import { FlatList, StyleSheet, View } from 'react-native'
 
-import {
-	useFonts,
-	AmaticSC_400Regular,
-	AmaticSC_700Bold,
-} from '@expo-google-fonts/amatic-sc'
-import * as SplashScreen from 'expo-splash-screen'
-import { useEffect } from 'react'
 import { DayListItem } from '../src/components/core/DayListItem'
 
-SplashScreen.preventAutoHideAsync()
-
 export default function Home() {
-	const [fontsLoaded, fontError] = useFonts({
-		Amatic: AmaticSC_400Regular,
-		AmaticBold: AmaticSC_700Bold,
-	})
-
-	useEffect(() => {
-		if (fontsLoaded || fontError) {
-			SplashScreen.hideAsync()
-		}
-	}, [fontsLoaded, fontError])
-
-	if (!fontsLoaded && !fontError) {
-		return null
-	}
-
 	return (
 		<View style={styles.container}>
 			<FlatList
