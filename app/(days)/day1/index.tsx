824ce1bfa1e6a4b3559b3f516cc0366649489354
Serhii <s.saviuk@apiko.com>
2024-01-23 15:18:14 +0200

    stack navigator

--- app/(days)/day1/index.tsx
+++ app/(days)/day1/index.tsx
@@ -1,8 +1,10 @@
+import { Stack } from 'expo-router'
 import { Text, View } from 'react-native'
 
 const DayDetailsScreen = () => {
 	return (
 		<View>
+			<Stack.Screen options={{ title: 'Day 1' }} />
 			<Text>Day1</Text>
 		</View>
 	)
